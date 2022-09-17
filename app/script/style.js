0c9a626a736deaed7fa30cd8be6ad6b852ff5d28
Esra ÇELİ <113723532+celiesra@users.noreply.github.com>
2022-09-18 01:02:47 +0300

    esra
    proje başlanıldı.

--- app/script/style.js
+++ app/script/style.js
@@ -7,6 +7,40 @@ $(document).ready(function () {
         elements_selector: ".lazyload"
     });
 
-    //Menu
+    //Home_Slider
+    const nexticon = "<i class=\"icon-left\"></i>";
+    const previcon = "<i class=\"icon-right\"></i>";
+
+    $('.home_slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots:true,
+        navText: [nexticon, previcon],
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            },
+            1200:{
+                items:1
+            },
+            1400:{
+                items:1
+            },
+            1600:{
+                items:1
+            },
+        }
+    });
+    
 
 });
